aa67c7506f2558322340d43f428fce8a7e173f66
José Guilherme Vanz <jvanz@jvanz.com>
2022-03-19 17:39:24 -0300

    Use mulitples thread to write files.
    Updates the download_wikipedia_data.py script using a Thread pool to
    write the tfrecord files.

--- scripts/download_wikipedia_data.py
+++ scripts/download_wikipedia_data.py
@@ -1,17 +1,20 @@
 import csv
 import os
+import string
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from concurrent.futures import as_completed
 
 import transformers
 from datasets import load_dataset
 import tensorflow as tf
 
 
-DATA_DIR = os.environ.get("DATA_DIR", "data/wikipedia")
+DATA_DIR = os.environ.get("DATA_DIR", "data")
 BATCH_SIZE = int(os.environ.get("BATCH_SIZE", 1000))
 
 
 def has_no_minimum_words(sentence):
-    return len(sentence.split(" ")) <= 3
+    return len(sentence.split(" ")) <= 5
 
 
 def has_invalid_char(sentence):
@@ -45,10 +48,9 @@ def serialize_sample(text):
     ).SerializeToString()
 
 
-def dataset_generator():
-    dataset_name = "wikipedia"
-    dataset_language = "pt"
-    dataset_date = "20220220"
+def get_wikipedia_dataset(
+    dataset_name="wikipedia", dataset_language="pt", dataset_date="20220220"
+):
     datasets = load_dataset(
         "wikipedia",
         language=dataset_language,
@@ -56,12 +58,34 @@ def dataset_generator():
         beam_runner="DirectRunner",
     )
     datasets = datasets.map(
-        sentence_segmentation, batched=True, remove_columns="title", num_proc=4
+        sentence_segmentation, batched=True, remove_columns="title", num_proc=6
     )
+    return datasets
+
+
+def dataset_generator():
+    dataset_name = "wikipedia"
+    dataset_language = "pt"
+    dataset_date = "20220220"
+    datasets = get_wikipedia_dataset(dataset_name, dataset_language, dataset_date)
     for dataset_name in datasets:
         for batch in datasets[dataset_name].to_dict(batched=True):
             for sample in batch["text"]:
                 yield (bytes(sample, "utf8"),)
+
+
+def write_wikipedia_file():
+    print("Writing Wikipedia file.")
+    dataset_name = "wikipedia"
+    dataset_language = "pt"
+    dataset_date = "20220220"
+    dataset = get_wikipedia_dataset(dataset_name, dataset_language, dataset_date)
+    dataset["train"].to_csv(
+        f"{DATA_DIR}/{dataset_name}_{dataset_date}_{dataset_language}.csv",
+        num_proc=6,
+        quoting=csv.QUOTE_ALL,
+        index=False,
+    )
 
 
 def load_sentence_dataset():
@@ -70,19 +94,47 @@ def load_sentence_dataset():
     )
 
 
+def write_tfrecord_file(filepath, batch):
+    print(f"Writing {filepath}")
+    with tf.io.TFRecordWriter(filepath) as file_writer:
+        for sample in batch[0]:
+            file_writer.write(serialize_sample(sample.numpy()))
+    return f"{filepath} written"
+
+
 def main():
-    sentences_dataset = load_sentence_dataset()
 
     if not os.path.exists(DATA_DIR):
         os.makedirs(DATA_DIR)
+    if not os.path.exists(f"{DATA_DIR}/wikipedia"):
+        os.makedirs(f"{DATA_DIR}/wikipedia")
 
-    counter = 0
-    for batch in sentences_dataset.batch(BATCH_SIZE):
-        filepath = f"{DATA_DIR}/{counter}_wikidata.tfrecords"
-        with tf.io.TFRecordWriter(filepath) as file_writer:
-            for sample in batch[0]:
-                file_writer.write(serialize_sample(sample.numpy()))
-        counter += 1
+    write_wikipedia_file()
+    results = []
+    with ThreadPoolExecutor(max_workers=6) as executor:
+        sentences_dataset = load_sentence_dataset()
+        counter = 0
+        for batch in sentences_dataset.batch(BATCH_SIZE):
+            results.append(
+                executor.submit(
+                    write_tfrecord_file,
+                    filepath=f"{DATA_DIR}/wikipedia/{counter}_wikidata.tfrecords",
+                    batch=batch,
+                )
+            )
+            counter += 1
+
+    total_files_written = 0
+    for result in results:
+        try:
+            result_returned = result.result()
+            if result_returned:
+                total_files_written += 1
+                print(result_returned)
+        except Exception as e:
+            print(e)
+
+    print("Total files written: {total_files_written}")
 
 
 if __name__ == "__main__":
