eac89ffa7457ea85b0d3b2c1bc89f877c4badeac
José Guilherme Vanz <jvanz@jvanz.com>
2022-06-06 19:41:17 -0300

    wip

--- test_bert_beneration.py
+++ test_bert_beneration.py
@@ -25,7 +25,7 @@ model.config.decoder_start_token_id = tokenizer.cls_token_id
 model.config.pad_token_id = tokenizer.pad_token_id
 model.config.vocab_size = model.config.decoder.vocab_size
 
-dataset = load_dataset("bookcorpus", streaming=False)
+dataset = load_dataset("bookcorpus", streaming=True)
 print(dataset)
 
 
@@ -37,22 +37,25 @@ def add_input_ids_and_labels(examples):
     return {"input_ids": examples["input_ids"], "labels": examples["input_ids"]}
 
 
-dataset = dataset.map(tokenize_function, batched=True, num_proc=os.cpu_count())
-dataset = dataset.map(add_input_ids_and_labels, batched=True, num_proc=os.cpu_count())
+# dataset = dataset.map(tokenize_function, batched=True, num_proc=os.cpu_count())
+dataset = dataset.map(tokenize_function, batched=True)
+# dataset = dataset.map(add_input_ids_and_labels, batched=True, num_proc=os.cpu_count())
+dataset = dataset.map(add_input_ids_and_labels, batched=True)
 dataset = dataset.remove_columns("text")
+dataset = dataset.with_format("torch")
 
 print("Dataset ready.")
-print(dataset["train"][0])
-print(next(iter(dataset["train"])))
-print(dataset["train"])
 
 print("Preparing datasets for training...")
-small_train_dataset = dataset["train"].shuffle(seed=42).select(range(dataset_size))
-small_eval_dataset = dataset["train"].shuffle(seed=42).select(range(dataset_size))
+# small_train_dataset = dataset["train"].shuffle(seed=42).select(range(dataset_size))
+# small_eval_dataset = dataset["train"].shuffle(seed=42).select(range(dataset_size))
+
+small_train_dataset = dataset["train"].shuffle(seed=42).take(dataset_size)
+small_eval_dataset = dataset["train"].shuffle(seed=42).take(dataset_size)
 print(small_train_dataset)
 
 
-training_args = TrainingArguments(output_dir="test_trainer")
+training_args = TrainingArguments(output_dir="test_trainer", max_steps=dataset_size / 8)
 metric = load_metric("accuracy")
 
 
@@ -65,11 +68,10 @@ def compute_metric(eval_pred):
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["train"],
+    train_dataset=small_train_dataset,
+    eval_dataset=small_eval_dataset,
     compute_metrics=compute_metric,
 )
 print(trainer.get_train_dataloader())
-print(len(trainer.get_train_dataloader()))
 
 trainer.train()
