695f4c3bce8f183f22ba8f32de57b8db98c6837c
José Guilherme Vanz <jvanz@jvanz.com>
2022-06-08 23:10:26 -0300

    wip

--- test_bert_beneration.py
+++ test_bert_beneration.py
@@ -33,12 +33,14 @@ model.config.decoder_start_token_id = tokenizer.cls_token_id
 model.config.pad_token_id = tokenizer.pad_token_id
 model.config.vocab_size = model.config.decoder.vocab_size
 
-dataset = load_dataset("bookcorpus", streaming=False)
+dataset = load_dataset("bookcorpus", streaming=False, split="train")
+dataset = dataset.train_test_split(shuffle=True)
+
 print(dataset)
-small_train_dataset = (
-    dataset["train"].shuffle(seed=42).select(range(train_dataset_size))
-)
-small_eval_dataset = dataset["train"].shuffle(seed=42).select(range(eval_dataset_size))
+# small_train_dataset = (
+#     dataset["train"].shuffle(seed=42).select(range(train_dataset_size))
+# )
+# small_eval_dataset = dataset["train"].shuffle(seed=42).select(range(eval_dataset_size))
 
 
 def tokenize_function(examples):
@@ -54,31 +56,31 @@ def add_input_ids_and_labels(examples):
     return {"input_ids": examples["input_ids"], "labels": examples["input_ids"]}
 
 
-small_train_dataset = small_train_dataset.map(
+dataset = dataset.map(
     tokenize_function, batched=True, num_proc=os.cpu_count()
 )
-# small_train_dataset = small_train_dataset.map(tokenize_function, batched=True)
-small_train_dataset = small_train_dataset.map(
+# dataset = dataset.map(tokenize_function, batched=True)
+dataset = dataset.map(
     add_input_ids_and_labels, batched=True, num_proc=os.cpu_count()
 )
-# small_train_dataset = small_train_dataset.map(add_input_ids_and_labels, batched=True)
-small_train_dataset = small_train_dataset.remove_columns("text")
-small_train_dataset = small_train_dataset.with_format("torch")
-
-small_eval_dataset = small_eval_dataset.map(
-    tokenize_function, batched=True, num_proc=os.cpu_count()
-)
-# small_eval_dataset = small_eval_dataset.map(tokenize_function, batched=True)
-small_eval_dataset = small_eval_dataset.map(
-    add_input_ids_and_labels, batched=True, num_proc=os.cpu_count()
-)
-# small_eval_dataset = small_eval_dataset.map(add_input_ids_and_labels, batched=True)
-small_eval_dataset = small_eval_dataset.remove_columns("text")
-small_eval_dataset = small_eval_dataset.with_format("torch")
+# dataset = dataset.map(add_input_ids_and_labels, batched=True)
+dataset = dataset.remove_columns("text")
+dataset = dataset.with_format("torch")
+#
+# small_eval_dataset = small_eval_dataset.map(
+#     tokenize_function, batched=True, num_proc=os.cpu_count()
+# )
+# # small_eval_dataset = small_eval_dataset.map(tokenize_function, batched=True)
+# small_eval_dataset = small_eval_dataset.map(
+#     add_input_ids_and_labels, batched=True, num_proc=os.cpu_count()
+# )
+# # small_eval_dataset = small_eval_dataset.map(add_input_ids_and_labels, batched=True)
+# small_eval_dataset = small_eval_dataset.remove_columns("text")
+# small_eval_dataset = small_eval_dataset.with_format("torch")
 
 print("Dataset ready.")
-print(small_train_dataset)
-print(small_eval_dataset)
+# print(small_train_dataset)
+# print(small_eval_dataset)
 
 training_args = TrainingArguments(
     output_dir=OUTPUT_DIR,
@@ -119,8 +121,8 @@ early_stop_callback = EarlyStoppingCallback(
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=small_train_dataset,
-    eval_dataset=small_eval_dataset,
+    train_dataset=dataset["train"],
+    eval_dataset=dataset["test"],
     # compute_metrics=compute_metric,
     callbacks=[early_stop_callback],
 )
